8005f3e09a29f4a1ed6ffb1061bebd4ba0b16064
Tzikas <niko.tzikas@gmail.com>
2020-01-10 17:04:43 -0500

    4 video.

--- script.js
+++ script.js
@@ -8,12 +8,22 @@ let school = 'ironhack'
 
 const logMyName = () => { //functions
     console.log(name + ' teaches at '+ school)
+
 }    
+
+const addASquare = () => {
+    var square = document.createElement('div')//    <div class="square">Square</div>
+    square.className += 'square'
+    document.body.appendChild(square)
+}
 
 
 //event listeners
-
 document.querySelector('i').onclick = logMyName;
 
+document.querySelector('.square').onclick = addASquare;
 
-
+setInterval(()=>{
+    console.log('hello')
+    addASquare()
+},1000)
